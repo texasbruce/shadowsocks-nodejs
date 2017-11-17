8ccc15c8f6ebbed82f0266885d3f5535b05850c6
texasbruce <bruce@texasbruce.com>
2017-11-17 15:56:34 +0800

    Added all type declaration for encrypt.ts

--- src/lib/encrypt.ts
+++ src/lib/encrypt.ts
@@ -2,13 +2,13 @@
   Copyright (c) 2014 clowwindy
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
+  of software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in
+  The above copyright notice and permission notice shall be included in
   all copies or substantial portions of the Software.
 
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -24,9 +24,11 @@
  * @author texasbruce
  */
 
-import crypto = require("crypto");
-import util = require("util");
-import merge_sort = require("./merge_sort");
+import crypto from "crypto";
+import util from "util";
+
+import {mergeSort} from "./merge_sort";
+import {Hash} from "./Hash";
 
 const INT32MAX = Math.pow(2, 32);
 const METHOD_SUPPORTED = {
@@ -47,233 +49,224 @@ const METHOD_SUPPORTED = {
 };
 
 
-export class Encryptor {
+type TablePair = [Array<number>, Array<number>];
+type BufferPair = [Buffer, Buffer];
+type Cipher = crypto.Cipher | crypto.Decipher;
 
-  private static bytes_to_key_results: any = {};
-  private static cachedTables = {};
-
-  private key: any;
-  private method: any;
-  private iv_sent: any;
-  private cipher: any;
-  private cipher_iv: any;
-  private encryptTable: any;
-  private decipher: any;
-  private decryptTable: any;
-
-  private static create_rc4_md5_cipher(key, iv, op) {
-    let md5, rc4_key;
-    md5 = crypto.createHash("md5");
-    md5.update(key);
-    md5.update(iv);
-    rc4_key = md5.digest();
-    if (op === 1) {
-      return crypto.createCipheriv("rc4", rc4_key, "");
-    } else {
-      return crypto.createDecipheriv("rc4", rc4_key, "");
-    }
-  }
-
-
-  public static getTable(key) {
-    let ah, al, decrypt_table, hash, i, md5sum, result, table;
-    if (this.cachedTables[key]) {
-      return this.cachedTables[key];
-    }
-    util.log("calculating ciphers");
-    table = new Array(256);
-    decrypt_table = new Array(256);
-    md5sum = crypto.createHash("md5");
-    md5sum.update(key);
-    hash = new Buffer(md5sum.digest(), "binary");
-    al = hash.readUInt32LE(0);
-    ah = hash.readUInt32LE(4);
-    i = 0;
-    while (i < 256) {
-      table[i] = i;
-      i++;
-    }
-    i = 1;
-    while (i < 1024) {
-      table = merge_sort.mergeSort(table, (x, y) => {
-        return ((ah % (x + i)) * INT32MAX + al) % (x + i) - ((ah % (y + i)) * INT32MAX + al) % (y + i);
-      });
-      i++;
-    }
-    i = 0;
-    while (i < 256) {
-      decrypt_table[table[i]] = i;
-      ++i;
-    }
-    result = [table, decrypt_table];
-    this.cachedTables[key] = result;
-    return result;
-  }
-
-
-  private static substitute(table, buf) {
-    let i = 0;
-    while (i < buf.length) {
-      buf[i] = table[buf[i]];
-      i++;
-    }
-    return buf;
-  }
-
-  private static EVP_BytesToKey(password, key_len, iv_len) {
-    let count, d, data, i, iv, key, m, md5, ms;
-    if (this.bytes_to_key_results["" + password + ":" + key_len + ":" + iv_len]) {
-      return this.bytes_to_key_results["" + password + ":" + key_len + ":" + iv_len];
-    }
-    m = [];
-    i = 0;
-    count = 0;
-    while (count < key_len + iv_len) {
-      md5 = crypto.createHash("md5");
-      data = password;
-      if (i > 0) {
-        data = Buffer.concat([m[i - 1], password]);
-      }
-      md5.update(data);
-      d = md5.digest();
-      m.push(d);
-      count += d.length;
-      i += 1;
-    }
-    ms = Buffer.concat(m);
-    key = ms.slice(0, key_len);
-    iv = ms.slice(key_len, key_len + iv_len);
-    this.bytes_to_key_results[password] = [key, iv];
-    return [key, iv];
-  }
-  public static encryptAll(password, method, op, data) {
-    let cipher, decryptTable, encryptTable, iv, ivLen, iv_, key, keyLen, result, _ref, _ref1, _ref2;
-    if (method === "table") {
-      method = null;
-    }
-    if (method == null) {
-      _ref = this.getTable(password), encryptTable = _ref[0], decryptTable = _ref[1];
-      if (op === 0) {
-        return this.substitute(decryptTable, data);
-      } else {
-        return this.substitute(encryptTable, data);
-      }
-    } else {
-      result = [];
-      method = method.toLowerCase();
-      _ref1 = METHOD_SUPPORTED[method], keyLen = _ref1[0], ivLen = _ref1[1];
-      password = new Buffer(password, "binary");
-      _ref2 = this.EVP_BytesToKey(password, keyLen, ivLen), key = _ref2[0], iv_ = _ref2[1];
-      if (op === 1) {
-        iv = crypto.randomBytes(ivLen);
-        result.push(iv);
-      } else {
-        iv = data.slice(0, ivLen);
-        data = data.slice(ivLen);
-      }
-      if (method === "rc4-md5") {
-        cipher = this.create_rc4_md5_cipher(key, iv, op);
-      } else {
-        if (op === 1) {
-          cipher = crypto.createCipheriv(method, key, iv);
-        } else {
-          cipher = crypto.createDecipheriv(method, key, iv);
-        }
-      }
-      result.push(cipher.update(data));
-      result.push(cipher.final());
-      return Buffer.concat(result);
-    }
-  }
-
-  constructor(key, method) {
-    let _ref;
-    this.key = key;
-    this.method = method;
-    this.iv_sent = false;
-    if (this.method === "table") {
-      this.method = null;
-    }
-    if (this.method != null) {
-      this.cipher = this.get_cipher(this.key, this.method, 1, crypto.randomBytes(32));
-    } else {
-      _ref = this.getClass().getTable(this.key), this.encryptTable = _ref[0], this.decryptTable = _ref[1];
-    }
-  }
-
-  public getClass(): typeof Encryptor {
-    return Encryptor;
-  }
-
-  private get_cipher(password, method, op, iv) {
-    let iv_, key, m, _ref;
-    method = method.toLowerCase();
-    password = new Buffer(password, "binary");
-    m = this.get_cipher_len(method);
-    if (m != null) {
-      _ref = this.getClass().EVP_BytesToKey(password, m[0], m[1]), key = _ref[0], iv_ = _ref[1];
-      if (iv == null) {
-        iv = iv_;
-      }
-      if (op === 1) {
-        this.cipher_iv = iv.slice(0, m[1]);
-      }
-      iv = iv.slice(0, m[1]);
-      if (method === "rc4-md5") {
-        return this.getClass().create_rc4_md5_cipher(key, iv, op);
-      } else {
-        if (op === 1) {
-          return crypto.createCipheriv(method, key, iv);
-        } else {
-          return crypto.createDecipheriv(method, key, iv);
-        }
-      }
-    }
-  }
-
-  private get_cipher_len(method) {
-    let m;
-    method = method.toLowerCase();
-    m = METHOD_SUPPORTED[method];
-    return m;
-  }
-
-  private encrypt(buf) {
-    let result;
-    if (this.method != null) {
-      result = this.cipher.update(buf);
-      if (this.iv_sent) {
-        return result;
-      } else {
-        this.iv_sent = true;
-        return Buffer.concat([this.cipher_iv, result]);
-      }
-    } else {
-      return this.getClass().substitute(this.encryptTable, buf);
-    }
-  }
-
-  private decrypt(buf) {
-    let decipher_iv, decipher_iv_len, result;
-    if (this.method != null) {
-      if (this.decipher == null) {
-        decipher_iv_len = this.get_cipher_len(this.method)[1];
-        decipher_iv = buf.slice(0, decipher_iv_len);
-        this.decipher = this.get_cipher(this.key, this.method, 0, decipher_iv);
-        result = this.decipher.update(buf.slice(decipher_iv_len));
-        return result;
-      } else {
-        result = this.decipher.update(buf);
-        return result;
-      }
-    } else {
-      return this.getClass().substitute(this.decryptTable, buf);
-    }
-  }
-
+interface Encryptor {
+  key: Buffer;
+  method: string;
+  iv_sent: boolean;
+  cipher_iv: Buffer;
+  cipher: Cipher;
+  decipher: Cipher;
+  encryptTable: Array<number>;
+  decryptTable: Array<number>;
 }
 
-export const encryptAll = Encryptor.encryptAll;
-export const getTable = Encryptor.getTable;
 
+const bytes_to_key_results: Hash<BufferPair> = {};
+const cachedTables: {[key: string]: TablePair} = {};
+
+function create_rc4_md5_cipher(key: Buffer, iv: Buffer, op: number): Cipher {
+  let md5 = crypto.createHash("md5").update(key).update(iv);
+  let rc4_key = md5.digest();
+  if (op === 1) {
+    return crypto.createCipheriv("rc4", rc4_key, "");
+  } else {
+    return crypto.createDecipheriv("rc4", rc4_key, "");
+  }
+}
+
+function getTable(key: Buffer): TablePair {
+
+  if (cachedTables[key.toString()]) {
+    return cachedTables[key.toString()];
+  }
+
+  util.log("calculating ciphers");
+
+  let table: Array<number> = new Array<number>(256);
+  let decrypt_table: Array<number> = new Array<number>(256);
+  let md5sum: crypto.Hash = crypto.createHash("md5").update(key);
+  let hash: Buffer = new Buffer(md5sum.digest().toString(), "binary");
+  let al: number = hash.readUInt32LE(0);
+  let ah: number = hash.readUInt32LE(4);
+
+  let i: number;
+  for (i = 0; i < 256; i++) {
+    table[i] = i;
+  }
+  // TODO What is going on here? Reassign table 1024 times?
+  for (i = 1; i < 1024; i++) {
+    table = mergeSort(table, (x, y) => {
+      return ((ah % (x + i)) * INT32MAX + al) % (x + i) - ((ah % (y + i)) * INT32MAX + al) % (y + i);
+    });
+  }
+  for (i = 0; i < 256; i++) {
+    decrypt_table[table[i]] = i;
+  }
+
+  return cachedTables[key.toString()] = [table, decrypt_table];
+}
+
+
+function substitute(table: Array<number>, buf: Buffer): Buffer{
+  for (let i = 0; i < buf.length; i++) {
+    buf[i] = table[buf[i]];
+  }
+  return buf;
+}
+
+function EVP_BytesToKey(password: Buffer, key_len: number, iv_len: number): BufferPair {
+  if (bytes_to_key_results["" + password + ":" + key_len + ":" + iv_len]) {
+    return bytes_to_key_results["" + password + ":" + key_len + ":" + iv_len];
+  }
+
+  let d: Buffer, data: Buffer, iv: Buffer, key: Buffer, md5: crypto.Hash, ms: Buffer;
+  let m: Buffer[] = [];
+  let i: number = 0;
+  let count: number = 0;
+  while (count < key_len + iv_len) {
+    md5 = crypto.createHash("md5");
+    data = password;
+    if (i > 0) {
+      data = Buffer.concat([m[i - 1], password]);
+    }
+    md5.update(data);
+    d = md5.digest();
+    m.push(d);
+    count += d.length;
+    i += 1;
+  }
+  ms = Buffer.concat(m);
+  key = ms.slice(0, key_len);
+  iv = ms.slice(key_len, key_len + iv_len);
+  bytes_to_key_results[password.toString()] = [key, iv];
+  return [key, iv];
+}
+function encryptAll(password: Buffer, method: string, op: number, data: Buffer): Buffer {
+  let cipher: Cipher, decryptTable: Array<number>, encryptTable: Array<number>, iv: Buffer, ivLen: number, 
+    iv_, key: Buffer, keyLen: number;
+  if (method === "table") {
+    method = null;
+  }
+  if (method == null) {
+    [encryptTable, decryptTable] = getTable(password);
+    if (op === 0) {
+      return substitute(decryptTable, data);
+    } else {
+      return substitute(encryptTable, data);
+    }
+  } else {
+    let result: Array<Buffer> = [];
+    method = method.toLowerCase();
+    [keyLen, ivLen] = METHOD_SUPPORTED[method];
+    password = new Buffer(password.toString(), "binary");
+    [key, iv_] = EVP_BytesToKey(password, keyLen, ivLen);
+    if (op === 1) {
+      iv = crypto.randomBytes(ivLen);
+      result.push(iv);
+    } else {
+      iv = data.slice(0, ivLen);
+      data = data.slice(ivLen);
+    }
+    if (method === "rc4-md5") {
+      cipher = create_rc4_md5_cipher(key, iv, op);
+    } else {
+      if (op === 1) {
+        cipher = crypto.createCipheriv(method, key, iv);
+      } else {
+        cipher = crypto.createDecipheriv(method, key, iv);
+      }
+    }
+    result.push(cipher.update(data));
+    result.push(cipher.final());
+    return Buffer.concat(result);
+  }
+}
+
+// TODO change property or local?
+function Encryptor(key: Buffer, method) : Encryptor {
+
+  let res: Encryptor = {} as any;
+
+  res.key = key;
+  res.method = method === "table" ? null : method;
+  res.iv_sent = false;
+
+  if (method != null) {
+    res.cipher = get_cipher(res, key, method, 1, crypto.randomBytes(32));
+  } else {
+    [res.encryptTable, res.decryptTable] = getTable(key);
+  }
+
+  return res;
+}
+
+function get_cipher(enc: Encryptor, password: Buffer, method: string, op: number, iv: Buffer) {
+  let iv_: Buffer, key: Buffer, m: Array<number>;
+  method = method.toLowerCase();
+  password = new Buffer(password.toString(), "binary");
+  m = get_cipher_len(method);
+  if (m != null) {
+    [key, iv_] = EVP_BytesToKey(password, m[0], m[1]);
+    if (iv == null) {
+      iv = iv_;
+    }
+    if (op === 1) {
+      enc.cipher_iv = iv.slice(0, m[1]);
+    }
+    iv = iv.slice(0, m[1]);
+    if (method === "rc4-md5") {
+      return create_rc4_md5_cipher(key, iv, op);
+    } else {
+      if (op === 1) {
+        return crypto.createCipheriv(method, key, iv);
+      } else {
+        return crypto.createDecipheriv(method, key, iv);
+      }
+    }
+  }
+}
+
+function get_cipher_len(method: string): Array<number> {
+  return METHOD_SUPPORTED[method.toLowerCase()];
+}
+
+function encrypt(enc: Encryptor, buf: Buffer): Buffer {
+  let result: Buffer;
+  if (enc.method != null) {
+    result = enc.cipher.update(buf);
+    if (enc.iv_sent) {
+      return result;
+    } else {
+      enc.iv_sent = true;
+      return Buffer.concat([enc.cipher_iv, result]);
+    }
+  } else {
+    return substitute(enc.encryptTable, buf);
+  }
+}
+
+function decrypt(enc: Encryptor, buf: Buffer): Buffer {
+  let decipher_iv: Buffer, decipher_iv_len: number, result: Buffer;
+  if (enc.method != null) {
+    if (enc.decipher == null) {
+      decipher_iv_len = get_cipher_len(enc.method)[1];
+      decipher_iv = buf.slice(0, decipher_iv_len);
+      enc.decipher = get_cipher(enc, enc.key, enc.method, 0, decipher_iv);
+      result = enc.decipher.update(buf.slice(decipher_iv_len));
+      return result;
+    } else {
+      result = enc.decipher.update(buf);
+      return result;
+    }
+  } else {
+    return substitute(enc.decryptTable, buf);
+  }
+}
+
+
+export {Encryptor, encryptAll, getTable};
 export default Encryptor;
